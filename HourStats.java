ab7657788aa5336ee22241fcf84d228c345f6e58
Zzcnick <zzhen@stuy.edu>
2015-11-29 00:02:40 -0500

    Add skeleton file

---
+++ HourStats.java
@@ -0,0 +1,11 @@
+// Hours Statistics Tool
+// Created by Zicheng Zhen
+
+import java.io.*;
+import java.nio.file.*;
+
+public class HourStats {
+    public static void main(String[] args) {
+	;
+    }
+}

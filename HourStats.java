a0e4ba528c47f044d92ca2b9eebb9240cf86bdf6
Zzcnick <zzhen@stuy.edu>
2015-11-29 01:37:19 -0500

    Finish display, needs ability for user input

--- HourStats.java
+++ HourStats.java
@@ -15,33 +15,60 @@ public class HourStats {
 		new FileReader(fileName);
 	    BufferedReader bufferedReader =
 		new BufferedReader(fileReader);
-	    String line = null;
-	    List<String[]> hourData = new ArrayList<String[]>();
+	    String line = bufferedReader.readLine(); // Skips first line
+	    line = bufferedReader.readLine(); // Skips second line
+	    List<Volunteer> hourData = new ArrayList<Volunteer>();
 	    while ((line = bufferedReader.readLine()) != null) {
 		String[] rawData = line.split(",");
-		String[] row = new String[]{rawData[0], rawData[1]};
+		String ID = rawData[0];
+		String hours = rawData[1];
+		Volunteer row = new Volunteer(ID, hours);
 		hourData.add(row);
 	    }
+	    Collections.sort(hourData, new checkVolunteer());
+	    
 	    print2DArray(hourData);
 	} catch (Exception ex) {
 	    System.out.println("ERROR: File not found.");
 	}
     }
 
-    private static void printRow(String[] a) {
+    private static void printRow(Volunteer v) {
 	String storage = "[";
-	for (String s : a) {
-	    storage += s + ",";
-	}
-	if (storage.equals("[")) {
-	    System.out.println(storage + "]");
-	} else {
-	    System.out.println(storage.substring(0,storage.length()-1) + "]");
+	storage += ("" + v.ID) + "," + ("" + v.hours) + "]";
+	System.out.println(storage);
+    }
+    private static void print2DArray(List<Volunteer> l) {
+	int i = 1;
+	for (Volunteer v : l) {
+	    printRow(v);
+	    i++;
+	    if (i > 10) {break;}
 	}
     }
-    private static void print2DArray(List<String[]> l) {
-	for (String[] s : l) {
-	    printRow(s);
+}
+
+class checkVolunteer implements Comparator<Volunteer> {
+    @Override
+    public int compare(Volunteer v1, Volunteer v2) {
+	if (v1.hours < v2.hours) {
+	    return 1;
+	} else {
+	    return -1;
 	}
+    }
+}
+
+class Volunteer {
+    public int ID = 0;
+    public double hours = 0;
+
+    public Volunteer() {
+	ID = 0;
+	hours = 0;
+    }
+    public Volunteer(String _id, String _hours) {
+	ID = Integer.parseInt(_id);
+	hours = Double.parseDouble(_hours);
     }
 }
